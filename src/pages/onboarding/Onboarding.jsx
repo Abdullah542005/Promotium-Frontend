04759da92e18f2e5b998cf445faaafddb2b1f371
ifaixal <fai.iqbal124@gmail.com>
2025-07-11 23:04:35 +0500

    Desktop Version Almost ready in term of visual There's still coding left after linking and sending the data to server

--- src/pages/onboarding/Onboarding.jsx
+++ src/pages/onboarding/Onboarding.jsx
@@ -29,7 +29,7 @@ export default function Onboarding() {
             </div>
             <div className="rightSection">
                 <div className="head-progress">
-                    <h3 className="BasicInfo">Basic Info</h3>
+                    <h3 className="BasicInfo">{stage === 1 ? 'Basic Info' : stage === 2 ? 'Link Social' : 'Terms & Conditions'}</h3>
                     <div className="progress">
                         <div className="line">
                         <div className="lin2"
@@ -76,8 +76,60 @@ export default function Onboarding() {
                         </div>
                     </div>
                     </>
-                )}  
+                    )}  
                 {/* Stage 1 Code Ended */}
+                    {stage === 2 && (
+                        <>
+                        <div className="socials">
+                            <div className="xTwitter Social">
+                                <div className="logo_text">
+                                    <svg width="6rem" height="6rem" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 1668.56 1221.19" viewBox="0 0 1668.56 1221.19" id="twitter-x">
+                                        <circle cx="834.28" cy="610.6" r="481.33"></circle>
+                                        <path fill="#fff" d="M485.39,356.79l230.07,307.62L483.94,914.52h52.11l202.7-218.98l163.77,218.98h177.32
+                                                    L836.82,589.6l215.5-232.81h-52.11L813.54,558.46L662.71,356.79H485.39z M562.02,395.17h81.46l359.72,480.97h-81.46L562.02,395.17
+                                                    z" transform="translate(52.39 -25.059)"></path>
+                                    </svg>
+                                    <p>X (Twitter)</p>
+                                </div>
+                                <button className="LinkXTwitter">Link</button>
+                                {/* After Linking Account the link button will change Color that needed to added later. */}
+                            </div>
+                            
+                            <div className="facebook Social">
+                                <div className="logo_text">
+                                    <svg width="3.25rem" height="3.25rem" className="fblogo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="facebook">
+                                        <path fill="#1877f2" d="M1024,512C1024,229.23016,794.76978,0,512,0S0,229.23016,0,512c0,255.554,187.231,467.37012,432,505.77777V660H302V512H432V399.2C432,270.87982,508.43854,200,625.38922,200,681.40765,200,740,210,740,210V336H675.43713C611.83508,336,592,375.46667,592,415.95728V512H734L711.3,660H592v357.77777C836.769,979.37012,1024,767.554,1024,512Z"></path>
+                                        <path fill="#fff" d="M711.3,660,734,512H592V415.95728C592,375.46667,611.83508,336,675.43713,336H740V210s-58.59235-10-114.61078-10C508.43854,200,432,270.87982,432,399.2V512H302V660H432v357.77777a517.39619,517.39619,0,0,0,160,0V660Z"></path>
+                                    </svg>
+                                    <p>Facebook</p>
+                                </div>
+                                <button className="LinkXFB">Link</button>
+                                {/* After Linking Account the link button will change Color that needed to added later. */}
+                            </div>
+                        </div>
+                        </>
+                        )}
+                        {/* Stage 2 Ended */}
+
+                        {stage === 3 && (
+                            <>
+                                <div className="TACPara">
+                                    <p className="Term_ConditionPara">
+                                        The platform Promotium ensures trustworthiness and legit experience for all of our users.
+                                        <br />
+                                        If a user is found cheating, using fake social media accounts, Sybil, exploiting bugs or by any means
+                                        damaging the platform directly or indirectly would be treated harshly which may include termination of
+                                        their accounts and slashing their stakes.
+                                    </p>
+                                    <div className="checkboxTC">
+                                        <div className="wrapperCheckbox">
+                                            <input type="checkbox" name="Terms_ConditionCheck" id="Terms_ConditionCheck" className="Terms_ConditionCheck" />
+                                        </div>
+                                        <label htmlFor="Terms_ConditionCheck">I Agree to Terms & Conditions</label>
+                                    </div>
+                                </div> 
+                            </>
+                        )}
 
                 <div className="buttons">
                     <button style={{cursor: stage === 1 ? 'not-allowed' : 'pointer', background: stage === 1 ? '#C5C5C5' : ''}} className="proceed back" onClick={() => {prevStage()}}>
