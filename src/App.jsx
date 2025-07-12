f4db88c801d65967793363a56c1f28d6863b72f1
Abdullah542005 <abdullahkhan542005@gmail.com>
2025-07-12 11:32:53 +0500

    Setupped Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Onboarding from './pages/onboarding/Onboarding'
+import Dashbaord from './pages/Dashboard/Dashboard'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div className='App'>
-      < Onboarding />
+      <Navbar />
+      <Routes>
+           <Route path="/onbording" element={<Onboarding/>}/>
+           <Route path='/' element={<Dashbaord />} />
+      </Routes>
     </div>
   )
 }
