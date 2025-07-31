9a4f03d53aac928bb8f5d33940bede216556da5b
ifaixal <fai.iqbal124@gmail.com>
2025-07-31 19:31:00 +0500

    SearchBar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -108,6 +108,7 @@ export default function Topbar() {
 
 
 function SearchMenu({closeMenu}){
+  const [selectedSearch, setSelectedSearch] = useState('Username');
   return(
     <div className="SearchMenu Menu">
           <div>
@@ -117,7 +118,46 @@ function SearchMenu({closeMenu}){
                 width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10.0303 8.96965C9.73741 8.67676 9.26253 8.67676 8.96964 8.96965C8.67675 9.26255 8.67675 9.73742 8.96964 10.0303L10.9393 12L8.96966 13.9697C8.67677 14.2625 8.67677 14.7374 8.96966 15.0303C9.26255 15.3232 9.73743 15.3232 10.0303 15.0303L12 13.0607L13.9696 15.0303C14.2625 15.3232 14.7374 15.3232 15.0303 15.0303C15.3232 14.7374 15.3232 14.2625 15.0303 13.9696L13.0606 12L15.0303 10.0303C15.3232 9.73744 15.3232 9.26257 15.0303 8.96968C14.7374 8.67678 14.2625 8.67678 13.9696 8.96968L12 10.9393L10.0303 8.96965Z" fill="#fafafa"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="#fafafa"></path> </g></svg>
             </div>
             <div className="SearchBar">
-              <input></input>
+              <div className="CustomInputWrapper">
+                {selectedSearch && (
+                  <div className="SearchBadge">
+                    {selectedSearch}
+                  </div>
+                )}
+                <input
+                  className="SearchInput"
+                  type="text"
+                  placeholder={`${selectedSearch} ID`}
+                />
+                <svg className="SearchSVGBTN" width="20px" height="20px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#ffffff" stroke-width="0.72" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>  
+              </div>
+
+            </div>
+
+            <div className="quickResult">
+              <h1>Quick Result</h1>
+              <div className="filter-group">
+                {['Username', 'Post', 'Report'].map((label) => (
+                  <label key={label} className={`filter-option ${selectedSearch === label ? 'selected' : ''}`}>
+                    <input
+                      type="radio"
+                      name="filter"
+                      value={label}
+                      checked={selectedSearch === label}
+                      onChange={() => setSelectedSearch(label)}
+                    />
+                    <div>{label}</div>
+                  </label>
+                ))}
+              </div>
+            </div>
+
+            <div className="ResultsSearch">
+              <h1>Result</h1>
+              <div className="usersResult">
+                <img width='30px' height='30px'/>
+                <p>@iFaixal</p>
+              </div>
             </div>
     </div>
   )
